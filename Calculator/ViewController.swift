ba088e443a316d5170cc61e5d540ee735bf0f23b
HYEZ <oohyejioo@gmail.com>
2020-01-21 03:20:01 +0900

    버튼 액션 추가

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -21,7 +21,59 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    var res = 0
+    
+    @IBAction func AC(_ sender: Any) {
+        
+    }
+    
+    @IBAction func Button1(_ sender: Any) {
+    }
+    
+    @IBAction func Button2(_ sender: Any) {
+    }
+    
+    @IBAction func Button3(_ sender: Any) {
+    }
+    
+    
+    @IBAction func Button4(_ sender: Any) {
+    }
+    
+    @IBAction func Button5(_ sender: Any) {
+    }
+    
+    @IBAction func Button6(_ sender: Any) {
+    }
+    
+    
+    @IBAction func Button7(_ sender: Any) {
+    }
+    
+    @IBAction func Button8(_ sender: Any) {
+    }
+    
+    
+    @IBAction func Button9(_ sender: Any) {
+    }
+    
+    @IBAction func Button0(_ sender: Any) {
+    }
+    
+    @IBAction func Button_div(_ sender: Any) {
+    }
+    
+    @IBAction func Button_mul(_ sender: Any) {
+    }
+    
+    @IBAction func Button_sub(_ sender: Any) {
+    }
+    
+    @IBAction func Button_sum(_ sender: Any) {
+    }
+    
+    @IBAction func result(_ sender: Any) {
+    }
 }
 
